5e33d3d86b882a02e947f6abf238adfcead39277
berghach <berghach.chemseddine@gmail.com>
2025-01-22 17:29:55 +0100

    TAAL-25 Starting to Set up the user management

---
+++ TaalomIT/src/main/java/com/example/TaalomIT/enumeration/Role.java
@@ -0,0 +1,7 @@
+package com.example.TaalomIT.enumeration;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_TEACHER,
+    ROLE_STUDENT
+}
---
+++ TaalomIT/src/main/java/com/example/TaalomIT/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.TaalomIT.repositories;
+
+import com.example.TaalomIT.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
