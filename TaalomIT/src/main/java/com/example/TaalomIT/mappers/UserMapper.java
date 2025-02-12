c7b58a370999fa07a392ed171904199d22d6502e
berghach <berghach.chemseddine@gmail.com>
2025-02-12 17:25:27 +0100

    TAAL-26 UserService is set

---
+++ TaalomIT/src/main/java/com/example/TaalomIT/mappers/UserMapper.java
@@ -0,0 +1,12 @@
+package com.example.TaalomIT.mappers;
+
+import com.example.TaalomIT.dtos.request.UserRequestDTO;
+import com.example.TaalomIT.dtos.response.UserResponseDTO;
+import com.example.TaalomIT.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface UserMapper {
+    User toEntity(UserRequestDTO userRequestDTO);
+    UserResponseDTO toResponseDTO(User user);
+}
