1da223e53905a4fc50fec87cb1c5d38f25ccc69a
berghach <berghach.chemseddine@gmail.com>
2025-02-21 15:40:31 +0100

    TAAL-29 auth-service is set

--- TaalomIT/src/main/java/com/example/TaalomIT/controllers/AuthController.java
+++ TaalomIT/src/main/java/com/example/TaalomIT/controllers/AuthController.java
@@ -16,16 +16,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthenticationManager authenticationManager;
--- taalom-ui/src/app/app.routes.ts
+++ taalom-ui/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth/auth.guard';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    
+    { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+];
