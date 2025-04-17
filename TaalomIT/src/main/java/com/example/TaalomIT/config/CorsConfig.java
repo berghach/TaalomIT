2eceb92c8ab23066f17673cfa681dafeb3899962
berghach <berghach.chemseddine@gmail.com>
2025-04-17 15:42:11 +0100

    uml edit, and deployement initial set up

--- TaalomIT/src/main/java/com/example/TaalomIT/config/CorsConfig.java
+++ TaalomIT/src/main/java/com/example/TaalomIT/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.TaalomIT.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,10 +13,13 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(frontendUrl));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
